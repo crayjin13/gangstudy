5964ff2315472714951bda64bccfd82979b717ec
apollyon4 <apollyon4@naver.com>
2020-09-10 14:36:09 +0900

    work2

--- src/main/java/com/jts/gangstudy/service/BookingService.java
+++ src/main/java/com/jts/gangstudy/service/BookingService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.jts.gangstudy.domain.Booking;
 
 public interface BookingService {
+	
+	// 예약 가능한 날짜 얻기
+	public void getAvaliableDates();
 	public List<Booking> viewAll();
 
 }
--- src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
+++ src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jts.gangstudy.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,31 @@ public class BookingServiceImpl implements BookingService{
 	
 	@Autowired
 	private BookingMapper mapper;
+	
+	private int avaliablePeriod = 7; // 현재 날짜로 부터 예약 가능한 기간
+	private String[][] avaliableDates = new String[avaliablePeriod][3]; // 예약 가능 기간의 년, 월, 일 배열
+	
+	private SimpleDateFormat yearFormat = new SimpleDateFormat ( "yyyy");
+	private SimpleDateFormat monthFormat = new SimpleDateFormat ( "MM");
+	private SimpleDateFormat dayFormat = new SimpleDateFormat ( "dd");
+	
+	private Calendar cal = Calendar.getInstance();
+	
+	@Override
+	public void getAvaliableDates() {
+		for(int i = 0; i < avaliablePeriod; i++) {
+			avaliableDates[i][0] = yearFormat.format(cal.getTime());
+			avaliableDates[i][1] = monthFormat.format(cal.getTime());
+			avaliableDates[i][2] = dayFormat.format(cal.getTime());
+	        cal.add(Calendar.DATE, 1);
+		}
+	}
 
 	@Override
 	public List<Booking> viewAll() {
+		getAvaliableDates();
 		return mapper.viewAll();
 	}
+
 
 }
