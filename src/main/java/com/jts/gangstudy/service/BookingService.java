a879541c6a55a147794b67bc614c51c5d62255a1
apollyon4 <apollyon4@naver.com>
2020-09-15 20:23:09 +0900

    booking duplicate handling

--- src/main/java/com/jts/gangstudy/service/BookingService.java
+++ src/main/java/com/jts/gangstudy/service/BookingService.java
@@ -8,12 +8,18 @@ import com.jts.gangstudy.domain.Booking;
 public interface BookingService {
 	// 전체 예약 목록 (디버깅용)
 	public List<Booking> viewAll();
-	
-	// 예약 가능한 시간 확인 (디버깅용)
-	public List<Booking> getExistBooking();
-	
+
 	// 선택 가능한 날짜 목록
 	public String[] getDateSelectOptions();
+	
+	// 해당 날짜의 예약 목록
+	public List<Booking> getDateBooking(String date);
+
+	// 가능한 예약 시작 시간
+	public List<String> getCITimes(List<Booking> books);
+	
+	// 가능한 예약 종료 시간
+	public List<String> getCOTimes(String ci, String[] options);
 	
 	// 예약 정보 생성
 	public Booking createBook(String book_dt, String ci, String co, int people);
--- src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
+++ src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
@@ -2,6 +2,7 @@ package com.jts.gangstudy.service;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -53,36 +54,70 @@ public class BookingServiceImpl implements BookingService{
 	
 	// 예약 가능한 날짜의 기존 예약 목록을 가져오는 함수
 	@Override
-	public List<Booking> getExistBooking() {
-		return mapper.viewDate("선택된 날짜를 줘야 한다.");
+	public List<Booking> getDateBooking(String date) {
+		return mapper.viewDate(date);
 	}
 	
 	// 해당 날짜의 예약 목록을 통해 그 날짜의 예약 가능한 시간을 구하는 함수
-	public ArrayList<String> getAvaliableTimes(List<Booking> books) {
-		ArrayList<String> times = new ArrayList<>();
+	@Override
+	public List<String> getCITimes(List<Booking> books) {
+		int hour, min;
+		String time;
+		List<String> times = new ArrayList<>();
+		
 		cal = Calendar.getInstance();
-		SimpleDateFormat hourFormat = new SimpleDateFormat("HH");
-		SimpleDateFormat minFormat = new SimpleDateFormat("mm");
 
 		// 예약 시간 단위로 모든 예약 시간 생성
 		for(cal.set(Calendar.HOUR_OF_DAY, start_hour), cal.set(Calendar.MINUTE, 0);
 			cal.get(Calendar.HOUR_OF_DAY) != end_hour;
-			cal.add(Calendar.MINUTE, minuteSize)) {
-			times.add(Calendar.HOUR_OF_DAY + ":" + Calendar.MINUTE);
+			cal.add(Calendar.MINUTE, minuteSize))
+		{
+			hour = cal.get(Calendar.HOUR_OF_DAY);
+			min = cal.get(Calendar.MINUTE);
+			times.add(String.format("%02d:%02d", hour, min));
 		}
+		
 		// 이미 예약된 시간 제거
-		int hour, min;
-		String time = null;
 		for(int i = 0; i < books.size(); i++) {
-			hour = Integer.parseInt(hourFormat.format(books.get(i).getCi()));
-			min = Integer.parseInt(minFormat.format(books.get(i).getCi()));
-			for(cal.set(Calendar.HOUR_OF_DAY, hour), cal.set(Calendar.HOUR_OF_DAY, min);
-				time != books.get(i).getCi();
-				cal.add(Calendar.HOUR_OF_DAY, minuteSize)) {
-				time = cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
-				times.remove(time);
-			}
+			hour = Integer.parseInt(books.get(i).getCi().substring(0, 2));
+			min = Integer.parseInt(books.get(i).getCi().substring(3, 5));
+			cal.set(Calendar.HOUR_OF_DAY, hour);
+			cal.set(Calendar.MINUTE, min);
+			do {
+				hour = cal.get(Calendar.HOUR_OF_DAY);
+				min = cal.get(Calendar.MINUTE);
+				time = String.format("%02d:%02d", hour, min);
+				cal.add(Calendar.MINUTE, minuteSize);
+				if(time.equals(books.get(i).getCo())) {
+					break;
+				}
+			} while(times.remove(time) != false);
 		}
+		return times;
+	}
+
+	@Override
+	public List<String> getCOTimes(String ci, String[] options) {
+		int hour, min;
+		String time;
+		List<String> times = new ArrayList<>();
+		hour = Integer.parseInt(ci.substring(0, 2));
+		min = Integer.parseInt(ci.substring(3, 5));
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, hour);
+		cal.set(Calendar.MINUTE, min);
+		List<String> ciList = Arrays.asList(options);
+		
+		// ci + minuteSize 부터 시작해서
+		// options에 해당 값이 있는동안 List<String>에 추가
+		do {
+			cal.add(Calendar.MINUTE, minuteSize);
+			hour = cal.get(Calendar.HOUR_OF_DAY);
+			min = cal.get(Calendar.MINUTE);
+			time = String.format("%02d:%02d", hour, min);
+			times.add(time);
+		} while(ciList.contains(time));
 		return times;
 	}
 	
