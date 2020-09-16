551e4d8d01d744dde40f27e9e19e019773e7e851
apollyon4 <apollyon4@naver.com>
2020-09-16 10:30:59 +0900

    e

--- src/main/java/com/jts/gangstudy/controller/BookingController.java
+++ src/main/java/com/jts/gangstudy/controller/BookingController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jts.gangstudy.domain.Booking;
@@ -26,19 +27,19 @@ public class BookingController {
 	public ModelAndView main() {
 		ModelAndView mav = new ModelAndView();
 		List<Booking> debug = bookingService.viewAll();
-		List<Booking> books = bookingService.getExistBooking();
 		String[] dates = bookingService.getDateSelectOptions();
+//		ArrayList<String> times = bookingService.get();
 		
 		mav.setViewName("booking");
 		mav.addObject("debug", debug);
-		mav.addObject("data", books);
 		mav.addObject("dates", dates);
+//		mav.addObject("times", times);
 		return mav;
 	}
 
 	@RequestMapping(value = "/request", method = RequestMethod.POST)
 	public String request(HttpServletRequest request) {
-		String book_dt = request.getParameter("date");
+		String book_dt = request.getParameter("book_dt");
 		String ci = request.getParameter("ci");
 		String co = request.getParameter("co");
 		int people = Integer.parseInt(request.getParameter("people"));
@@ -46,5 +47,21 @@ public class BookingController {
 		bookingService.insertBook(book);
 		return "redirect:/booking";
 	}
-
+	
+	@ResponseBody
+	@RequestMapping(value = "/getCI", method = RequestMethod.GET)
+	public List<String> getCI(HttpServletRequest request) {
+		String date = request.getParameter("date");
+		List<Booking> books = bookingService.getDateBooking(date);
+		List<String> times = bookingService.getCITimes(books);
+		return times;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/getCO", method = RequestMethod.GET)
+	public List<String> getCO(HttpServletRequest request, String[] options) {
+		String ci = request.getParameter("ci");
+		List<String> times = bookingService.getCOTimes(ci, options);
+		return times;
+	}
 }
