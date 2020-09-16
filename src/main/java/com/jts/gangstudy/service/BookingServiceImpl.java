61640833b7c529208c0f2e783d22fc2c29f9c942
JTS <crayjin13>
2020-09-16 12:06:47 +0900

    -- bookingServiceimpl 파라미터값 수정

--- src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
+++ src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
@@ -20,12 +20,12 @@ public class BookingServiceImpl implements BookingService{
 	private BookingMapper mapper;
 	private Calendar cal;
 	
-	private int dateSize = 7;			// 예약 신청이 가능한 기간
-	private int minuteSize = 30;		// 에약 신청 시간 단위
-	private int start_hour = 9;		// 예약 가능한 시작 시간
-	private int end_hour = 23;			// 예약 가능한 종료 시간
+	private int dateSize = 7;			// �삁�빟 �떊泥��씠 媛��뒫�븳 湲곌컙
+	private int minuteSize = 30;		// �뿉�빟 �떊泥� �떆媛� �떒�쐞
+	private int start_hour = 9;		// �삁�빟 媛��뒫�븳 �떆�옉 �떆媛�
+	private int end_hour = 23;			// �삁�빟 媛��뒫�븳 醫낅즺 �떆媛�
 	
-	// 예약 가능한 날짜 배열 구하는 함수
+	// �삁�빟 媛��뒫�븳 �궇吏� 諛곗뿴 援ы븯�뒗 �븿�닔
 	public String[] getAvaliableDates() {
 		cal = Calendar.getInstance();
 		SimpleDateFormat calFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -38,7 +38,7 @@ public class BookingServiceImpl implements BookingService{
 		return dates;
 	}
 	
-//	// 예약 가능한 날짜의 시작일, 종료일을 구하는 함수
+//	// �삁�빟 媛��뒫�븳 �궇吏쒖쓽 �떆�옉�씪, 醫낅즺�씪�쓣 援ы븯�뒗 �븿�닔
 //	public HashMap<String, String> getBookingRange() {
 //		String start_dt, end_dt;
 //		String[] avaliableDates = getAvaliableDates();
@@ -52,13 +52,13 @@ public class BookingServiceImpl implements BookingService{
 //		return range;
 //	}
 	
-	// 예약 가능한 날짜의 기존 예약 목록을 가져오는 함수
+	// �삁�빟 媛��뒫�븳 �궇吏쒖쓽 湲곗〈 �삁�빟 紐⑸줉�쓣 媛��졇�삤�뒗 �븿�닔
 	@Override
 	public List<Booking> getDateBooking(String date) {
 		return mapper.viewDate(date);
 	}
 	
-	// 해당 날짜의 예약 목록을 통해 그 날짜의 예약 가능한 시간을 구하는 함수
+	// �빐�떦 �궇吏쒖쓽 �삁�빟 紐⑸줉�쓣 �넻�빐 洹� �궇吏쒖쓽 �삁�빟 媛��뒫�븳 �떆媛꾩쓣 援ы븯�뒗 �븿�닔
 	@Override
 	public List<String> getCITimes(List<Booking> books) {
 		int hour, min;
@@ -67,7 +67,7 @@ public class BookingServiceImpl implements BookingService{
 		
 		cal = Calendar.getInstance();
 
-		// 예약 시간 단위로 모든 예약 시간 생성
+		// �삁�빟 �떆媛� �떒�쐞濡� 紐⑤뱺 �삁�빟 �떆媛� �깮�꽦
 		for(cal.set(Calendar.HOUR_OF_DAY, start_hour), cal.set(Calendar.MINUTE, 0);
 			cal.get(Calendar.HOUR_OF_DAY) != end_hour;
 			cal.add(Calendar.MINUTE, minuteSize))
@@ -77,7 +77,7 @@ public class BookingServiceImpl implements BookingService{
 			times.add(String.format("%02d:%02d", hour, min));
 		}
 		
-		// 이미 예약된 시간 제거
+		// �씠誘� �삁�빟�맂 �떆媛� �젣嫄�
 		for(int i = 0; i < books.size(); i++) {
 			hour = Integer.parseInt(books.get(i).getCi().substring(0, 2));
 			min = Integer.parseInt(books.get(i).getCi().substring(3, 5));
@@ -109,8 +109,8 @@ public class BookingServiceImpl implements BookingService{
 		cal.set(Calendar.MINUTE, min);
 		List<String> ciList = Arrays.asList(options);
 		
-		// ci + minuteSize 부터 시작해서
-		// options에 해당 값이 있는동안 List<String>에 추가
+		// ci + minuteSize 遺��꽣 �떆�옉�빐�꽌
+		// options�뿉 �빐�떦 媛믪씠 �엳�뒗�룞�븞 List<String>�뿉 異붽�
 		do {
 			cal.add(Calendar.MINUTE, minuteSize);
 			hour = cal.get(Calendar.HOUR_OF_DAY);
@@ -121,25 +121,25 @@ public class BookingServiceImpl implements BookingService{
 		return times;
 	}
 	
-	// 전체 예약 목록 (디버깅용)
+	// �쟾泥� �삁�빟 紐⑸줉 (�뵒踰꾧퉭�슜)
 	@Override
 	public List<Booking> viewAll() {
 		return mapper.viewAll();
 	}
-	// 예약 정보 생성
+	// �삁�빟 �젙蹂� �깮�꽦
 	@Override
 	public Booking createBook(String book_dt, String ci, String co, int people) {
-		return new Booking(22, 1, book_dt, ci, co, people, "wait");
+		return new Booking(1, 1, book_dt, ci, co, people, "wait");
 	}
 
 	@Override
-	// 예약 추가
+	// �삁�빟 異붽�
 	public void insertBook(Booking book) {
 		mapper.insertBook(book);
 	}
 
 	@Override
-	// 선택 가능한 날짜 목록
+	// �꽑�깮 媛��뒫�븳 �궇吏� 紐⑸줉
 	public String[] getDateSelectOptions() {
 		return getAvaliableDates();
 	}
