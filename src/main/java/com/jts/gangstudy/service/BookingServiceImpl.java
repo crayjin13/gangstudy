479376934a21ee2a91c2b62c9ab8c13e46fed135
apollyon4 <apollyon4@naver.com>
2020-09-17 16:24:24 +0900

    error fix

--- src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
+++ src/main/java/com/jts/gangstudy/service/BookingServiceImpl.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,125 +20,105 @@ public class BookingServiceImpl implements BookingService{
 	private Calendar cal;
 	
 	private int dateSize = 7;			// 예약 신청이 가능한 기간
-	private int minuteSize = 30;		// 에약 신청 시간 단위
-	private int start_hour = 9;			// 예약 가능한 시작 시간
-	private int end_hour = 23;			// 예약 가능한 종료 시간
-	
-	// 예약 가능한 날짜 배열 구하는 함수
-	public String[] getAvaliableDates() {
-		cal = Calendar.getInstance();
-		SimpleDateFormat calFormat = new SimpleDateFormat("yyyy-MM-dd");
-		String[] dates = new String[dateSize];
-		
-		for(int i = 0; i < dateSize; i++) {
-			dates[i] = calFormat.format(cal.getTime());
-	        cal.add(Calendar.DATE, 1);
-		}
-		return dates;
-	}
-	
-//	public HashMap<String, String> getBookingRange() {
-//		String start_dt, end_dt;
-//		String[] avaliableDates = getAvaliableDates();
-//		start_dt = avaliableDates[0];
-//		end_dt = avaliableDates[dateSize-1];
-//
-//		HashMap<String, String> range = new HashMap<String, String>(); 
-//		range.put("start_dt", start_dt);
-//		range.put("end_dt", end_dt);
-//		
-//		return range;
-//	}
-	
-	// 예약 가능한 날짜의 기존 예약 목록을 가져오는 함수
-	@Override
-	public List<Booking> getDateBooking(String date) {
-		return mapper.viewDate(date);
-	}
-	
-	// 해당 날짜의 예약 목록을 통해 그 날짜의 예약 가능한 시간을 구하는 함수
-	@Override
-	public List<String> getCITimes(List<Booking> books) {
-		int hour, min;
-		String time;
-		List<String> times = new ArrayList<>();
-		
-		cal = Calendar.getInstance();
+	private int minuteSize = 30;			// 에약 신청 시간 단위
+	private int startHour = 9;			// 예약 가능한 시작 시간
+	private int endHour = 23;			// 예약 가능한 종료 시간
 
-		// 예약 시간 단위로 모든 예약 시간 생성
-		for(cal.set(Calendar.HOUR_OF_DAY, start_hour), cal.set(Calendar.MINUTE, 0);
-			cal.get(Calendar.HOUR_OF_DAY) != end_hour;
-			cal.add(Calendar.MINUTE, minuteSize))
-		{
-			hour = cal.get(Calendar.HOUR_OF_DAY);
-			min = cal.get(Calendar.MINUTE);
-			times.add(String.format("%02d:%02d", hour, min));
-		}
-		
-		// 이미 예약된 시간 제거
-		for(int i = 0; i < books.size(); i++) {
-			hour = Integer.parseInt(books.get(i).getCi().substring(0, 2));
-			min = Integer.parseInt(books.get(i).getCi().substring(3, 5));
-			cal.set(Calendar.HOUR_OF_DAY, hour);
-			cal.set(Calendar.MINUTE, min);
-			do {
-				hour = cal.get(Calendar.HOUR_OF_DAY);
-				min = cal.get(Calendar.MINUTE);
-				time = String.format("%02d:%02d", hour, min);
-				cal.add(Calendar.MINUTE, minuteSize);
-				if(time.equals(books.get(i).getCo())) {
-					break;
-				}
-			} while(times.remove(time) != false);
-		}
-		return times;
-	}
-
-	@Override
-	public List<String> getCOTimes(String ci, String[] options) {
-		int hour, min;
-		String time;
-		List<String> times = new ArrayList<>();
-		hour = Integer.parseInt(ci.substring(0, 2));
-		min = Integer.parseInt(ci.substring(3, 5));
-		
-		cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY, hour);
-		cal.set(Calendar.MINUTE, min);
-		List<String> ciList = Arrays.asList(options);
-		
-		// ci + minuteSize 부터 시작해서
-		// options에 해당 값이 있는동안 List<String>에 추가
-		do {
-			cal.add(Calendar.MINUTE, minuteSize);
-			hour = cal.get(Calendar.HOUR_OF_DAY);
-			min = cal.get(Calendar.MINUTE);
-			time = String.format("%02d:%02d", hour, min);
-			times.add(time);
-		} while(ciList.contains(time));
-		return times;
-	}
-	
 	// 전체 예약 목록 (디버깅용)
 	@Override
 	public List<Booking> viewAll() {
 		return mapper.viewAll();
 	}
-	// 예약 정보 생성
+
+	// 선택 가능한 날짜 목록
 	@Override
-	public Booking createBook(String book_dt, String ci, String co, int people) {
-		return new Booking(22, 1, book_dt, ci, co, people, "wait");
+	public List<String> getDateSelectOptions() {
+		return getDates(dateSize);
 	}
 
+	// 해당 날짜의 예약 목록
 	@Override
+	public List<Booking> getDateBooking(String date) {
+		return mapper.viewDate(date);
+	}
+	
+	// 시작일 기준의 시작시간 목록
+	@Override
+	public List<String> getCITimes(List<Booking> books) {
+		// 예약 시간 단위로 모든 예약 시간 생성
+		List<String> ciList = getTimes(startHour, endHour, minuteSize);
+		
+		// 기존 예약을 예약 시간에서 제거
+		for(Booking book : books) {
+			removeTimes(ciList, book.getCi(), book.getCo(), minuteSize);
+		}
+		return ciList;
+	}
+
+	// 시작시간 기준의 종료시간 목록
+	@Override
+	public List<String> getCOTimes(String ci, String[] ciArray) {
+		String time;
+		List<String> coList = new ArrayList<>();
+		List<String> ciList = Arrays.asList(ciArray);
+
+		int hour = Booking.getHour(ci);
+		int min = Booking.getMinute(ci);
+		cal = Calendar.getInstance();
+		Booking.setTime(cal, hour, min);
+		
+		// 종료 시간 추가
+		do {
+			cal.add(Calendar.MINUTE, minuteSize);
+			time = Booking.getTime(cal);
+			coList.add(time);
+		} while(ciList.contains(time));
+		return coList;
+	}
+
 	// 예약 추가
+	@Override
 	public void insertBook(Booking book) {
 		mapper.insertBook(book);
 	}
 
-	@Override
-	// 선택 가능한 날짜 목록
-	public String[] getDateSelectOptions() {
-		return getAvaliableDates();
+	// date 목록 반
+	public List<String> getDates(int dateSize) {
+		SimpleDateFormat calFormat = new SimpleDateFormat("yyyy-MM-dd");
+		List<String> dates = new ArrayList<String>();
+		Calendar cal = Calendar.getInstance();
+		
+		for(int i = 0; i < dateSize; i++) {
+			dates.add(calFormat.format(cal.getTime()));
+	        cal.add(Calendar.DATE, 1);
+		}
+		return dates;
+	}
+
+	// time 목록 반환
+	public List<String> getTimes(int start, int end, int minuteSize) {
+		List<String> times = new ArrayList<>();
+		String time;
+		Calendar cal = Calendar.getInstance();
+		
+		for(Booking.setTime(cal, start, 0); cal.get(Calendar.HOUR_OF_DAY) != end; cal.add(Calendar.MINUTE, minuteSize)) {
+			time = Booking.getTime(cal);
+			times.add(time);
+		}
+		return times;
+	}
+	
+	// 일정구간 예약을 지우는 함수
+	public void removeTimes(List<String> times, String start, String end, int minuteSize){
+		int hour, min;
+		Calendar cal = Calendar.getInstance();
+		
+		hour = Booking.getHour(start);
+		min = Booking.getMinute(start);
+		Booking.setTime(cal, hour, min);
+			
+		for(String time = Booking.getTime(cal); !time.equals(end); cal.add(Calendar.MINUTE, minuteSize), time = Booking.getTime(cal)) {
+			times.remove(time);
+		}
 	}
 }
