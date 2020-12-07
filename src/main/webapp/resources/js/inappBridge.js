aa45cd911a92be820565d76df5efec131ce41160
apollyon4 <apollyon4@naver.com>
2020-12-07 10:46:20 +0900

    naver app android bridge add.

--- src/main/webapp/resources/js/inappBridge.js
+++ src/main/webapp/resources/js/inappBridge.js
@@ -4,13 +4,18 @@
 // Mobile inApp browser(naver, kakao...) to External browser(chrome, safari...)
 (function() {
 	var userAgent = navigator.userAgent;
-	if (userAgent.match(/KAKAO|NAVER/i)) {					// inapp
+	if (userAgent.match(/KAKAO/i)) {					// inapp |NAVER
 		if(userAgent.match(/Android/i)) {					// android
 			location.href='kakaotalk://inappbrowser/close'
 			location.href='intent://gangstudy.com/#Intent;scheme=http;package=com.android.chrome;end'
 		} else if (userAgent.match(/iPhone|iPad|iPod/i)) {	// ios
 //			location.href='kakaoweb://closeBrowser'
 //			location.href='ftp://15.165.185.2/pub/bridge.html'
+		}
+	} else if(userAgent.match(/NAVER/i)) {
+		if(userAgent.match(/Android/i)) {					// android
+			location.href='intent://gangstudy.com/#Intent;scheme=http;package=com.android.chrome;end'
+			location.href='https://naver.com/'
 		}
 	} else if(document.location.hostname == "ec2-15-165-185-2.ap-northeast-2.compute.amazonaws.com") {
 		location.href="http://gangstudy.com"
