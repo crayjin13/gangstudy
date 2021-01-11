72bb7370097a83b38af03ec290a21d0ddeb03d1d
crayjin13 <60996449+crayjin13@users.noreply.github.com>
2021-01-11 11:12:12 +0900

    아임포트 결제후 북킹리스트 페이지 3초간견으로 새로고침 (uncharge로 저장되는것 새로고침하기위해)

--- src/main/webapp/WEB-INF/views/booking/bookingList.jsp
+++ src/main/webapp/WEB-INF/views/booking/bookingList.jsp
@@ -15,7 +15,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!--begin::Head-->  
 	<head><base href="../../../">
 		<meta charset="utf-8" />
-		<title>예약내역 | gangstudy</title>
+		<title>예약내역 | gangstudy</title>    				<META HTTP-EQUIV="refresh" CONTENT="3">
 		<meta name="description" content="Javascript sourced data examples" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="canonical" href="https://keenthemes.com/metronic" />
@@ -260,7 +260,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
 		<!--begin::Global Config(global config for global JS scripts)-->
 		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
-		<!--end::Global Config-->
+		<!--end::Global Config-->		<script src="https://code.jquery.com/jquery-3.3.1.min.js"    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="   crossorigin="anonymous"></script><!-- jQuery CDN --->
 		<!--begin::Global Theme Bundle(used by all pages)-->
 		<script src="${pageContext.request.contextPath}/resources/assets/plugins/global/plugins.bundle.js"></script>
 		<script src="${pageContext.request.contextPath}/resources/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
--- src/main/webapp/resources/assets/js/iamport.js
+++ src/main/webapp/resources/assets/js/iamport.js
@@ -1,89 +1,86 @@
-   
-  $(function(){
-	  $('#pay').on('click',function(e){
-        var IMP = window.IMP; // 생략가능
-        IMP.init('imp20137588'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
-        var msg;
-        const pointUse = document.getElementById("kt_touchspin");
-        const people = document.getElementById("touchspin");
-        
-      $.post("/booking/paybyDanal", {
-            people : people.value,
-            point : pointUse.value
-    	},function(jqXHR) {        
-    		// always
-    		
-    	},'text' /* xml, text, script, html*/ )
-    	.done(function(data) {
-    		merchant_uid = data;     
-    	  
-    	         
-             console.log(data);    
-             
-        
-             // begin: IMP.request_pay 다날  결제화면으로 넘어가는 부분 
-        IMP.request_pay({  
-            pg : 'Danal',
-            pay_method : 'card',
-            merchant_uid : merchant_uid  /* + new Date().getTime()*/,
-            name : '갱스터디',
-            amount : document.getElementById("totalAmount").textContent,     
-            buyer_email : '' ,
-            buyer_name : document.getElementById("name").textContent, 
-            buyer_tel :'' ,
-            //m_redirect_url : 'http://www.naver.com' 모바일 결제시, 결제가 끝나고 랜딩되는 URL을 지정 (카카오페이, 페이코, 다날의 경우는 필요없음. PC와 마찬가지로 callback함수로 결과가 떨어짐) 
-        }, function(rsp) {
-            if ( rsp.success ) {
-                //[1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
-            	console.log('2 차 지나고 ');
-                jQuery.ajax({
-                    url: "/payment/beready", //cross-domain error가 발생하지 않도록 주의해주세요
-                    type: 'GET',
-                    dataType: 'json',
-                    data: {                   
-                        imp_uid : rsp.imp_uid
-                        //기타 필요한 데이터가 있으면 추가 전달
-                    }
-                }).done(function(data) {
-                    //[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우  
-                	
-                	console.log('3차 지나고 ');
-                    if ( everythings_fine ) {
-                      var  msg = '결제가 완료되었습니다.';
-                        msg += '\n고유ID : ' + rsp.imp_uid;
-                        msg += '\n상점 거래ID : ' + rsp.merchant_uid;
-                        msg += '\결제 금액 : ' + rsp.paid_amount;
-                        msg += '카드 승인번호 : ' + rsp.apply_num;
-                        
-                        console.log(rsp.merchant_uid);
-                        alert(msg);
-                    } else {
-                    	msg = +rsp.error_msg;
-                    	alert('결제에 오류가 있습니다. ');
-                        //[3] 아직 제대로 결제가 되지 않았습니다.
-                        //[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
-                    }
-                });
-                //성공시 이동할 페이지
-               	//location.href='<%=request.getContextPath()%>/order/paySuccess?msg='+msg;
-           //     location.href='<%=request.getContextPath()%>/order-details';
-                	
-                	
-                alert('결제가 완료되었습니다');
-                  window.location.href='/booking/check';             
-                 
-            } else {
-                msg = '결제에 실패하였습니다.';
-                msg += '에러내용 : ' + rsp.error_msg;
-              //  window.location.href='/';
-                //실패시 이동할 페이지
-               // location.href="<%=request.getContextPath()%>/order/payFail";
-                alert(msg);
-            }
-        });   // end: IMP.request_pay 다날  결제화면 종료
-        
-    	});  // end: 비동기 $.post( paybydanal) 매소드 종료 
-        
-	  });   // end: #pay button 클릭했을떄 실행되는 메소드 
+$(function() {
+	$('#pay').on('click', function(e) {
+		var IMP = window.IMP; // 생략가능
+		IMP.init('imp20137588'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+		var msg;
+		const pointUse = document.getElementById("kt_touchspin");
+		const people = document.getElementById("touchspin");
 
-    });
+		$.post("/booking/paybyDanal", {
+			people : people.value,
+			point : pointUse.value
+		}, function(jqXHR) {
+			// always
+
+		}, 'text' /* xml, text, script, html */).done(function(data) {
+			merchant_uid = data;
+
+			console.log(data);
+
+			// begin: IMP.request_pay 다날 결제화면으로 넘어가는 부분
+			IMP.request_pay({
+				pg : 'Danal',
+				pay_method : 'card',
+				merchant_uid : merchant_uid /* + new Date().getTime() */,
+				name : '갱스터디',
+				amount : document.getElementById("totalAmount").textContent,
+				buyer_email : '',
+				buyer_name : document.getElementById("name").textContent,
+				buyer_tel : '',
+			// m_redirect_url : 'http://www.naver.com' 모바일 결제시, 결제가 끝나고 랜딩되는
+			// URL을 지정 (카카오페이, 페이코, 다날의 경우는 필요없음. PC와 마찬가지로 callback함수로 결과가 떨어짐)
+			}, function(rsp) {
+				if (rsp.success) {
+					// [1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
+					console.log('2 차 지나고 ');
+					jQuery.ajax({
+						url : "/payment/beready", // cross-domain error가 발생하지
+													// 않도록 주의해주세요
+						type : 'GET',
+						dataType : 'json',
+						data : {
+							imp_uid : rsp.imp_uid
+						// 기타 필요한 데이터가 있으면 추가 전달
+						}
+					}).done(function(data) {
+						// [2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
+
+						console.log('3차 지나고 ');
+						if (everythings_fine) {
+							var msg = '결제가 완료되었습니다.';
+							msg += '\n고유ID : ' + rsp.imp_uid;
+							msg += '\n상점 거래ID : ' + rsp.merchant_uid;
+							msg += '\결제 금액 : ' + rsp.paid_amount;
+							msg += '카드 승인번호 : ' + rsp.apply_num;
+
+							console.log(rsp.merchant_uid);
+							alert(msg);
+						} else {
+							msg = +rsp.error_msg;
+							alert('결제에 오류가 있습니다. ');
+							// [3] 아직 제대로 결제가 되지 않았습니다.
+							// [4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
+						}
+					});
+					// 성공시 이동할 페이지
+					// location.href='<%=request.getContextPath()%>/order/paySuccess?msg='+msg;
+					
+
+					alert('결제가 완료되었습니다'); // ok 클릭시 여기서 마지막 컨트롤러 진행됨.
+					window.location.href = '/booking/check';
+				} else {        
+					msg = '결제에 실패하였습니다.';
+					msg += '에러내용 : ' + rsp.error_msg;
+					// window.location.href='/';
+					// 실패시 이동할 페이지
+					// location.href="<%=request.getContextPath()%>/order/payFail";
+					alert(msg);
+				}
+			}); // end: IMP.request_pay 다날 결제화면 종료   
+
+		}); // end: 비동기 $.post( paybydanal) 매소드 종료
+	      
+
+	         
+	}); // end: #pay button 클릭했을떄 실행되는 메소드
+});
