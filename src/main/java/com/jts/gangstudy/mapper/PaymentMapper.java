da8d01e053f02f54b84bc447a97e6e5d73aec3a1
crayjin13 <60996449+crayjin13@users.noreply.github.com>
2021-01-12 13:02:50 +0900

    Revert "Revert "아임포트 rest api 코드추가 + pg사 체크하는 메소드""
    This reverts commit 2ef688290ddd6e9fb88fb5bb06a75425b23c4a9a.

--- src/main/java/com/jts/gangstudy/mapper/PaymentMapper.java
+++ src/main/java/com/jts/gangstudy/mapper/PaymentMapper.java
@@ -8,4 +8,5 @@ public interface PaymentMapper {
 	public void insertPayment(Payment payment);
 	public Payment selectPayment(int book_no);
 	public void updateState(HashMap<String, String> map);
+	public int pgDanalCheck(int book_no);
 }
