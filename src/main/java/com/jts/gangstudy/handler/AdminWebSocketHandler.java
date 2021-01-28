e47da193886e1ce9803f874dbc16905ef596f45d
apollyon4 <apollyon4@naver.com>
2021-01-28 11:15:04 +0900

    mms call

--- src/main/java/com/jts/gangstudy/handler/AdminWebSocketHandler.java
+++ src/main/java/com/jts/gangstudy/handler/AdminWebSocketHandler.java
@@ -25,6 +25,7 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.jts.gangstudy.domain.RemoteLog;
+import com.jts.gangstudy.domain.SendMmsMessage;
 import com.jts.gangstudy.service.AdminService;
 
 public class AdminWebSocketHandler extends TextWebSocketHandler implements InitializingBean, DisposableBean {
@@ -78,10 +79,16 @@ public class AdminWebSocketHandler extends TextWebSocketHandler implements Initi
 						continue;
 					} else if(msg.equals("<M1>")) {
 						// 문자 요청
+						String text = "[* 갱스터디 *] M1 !  즉시 지원 바랍니다. ";   // [필수] 문자내용 - 이름(names)값이 있다면 [*이름*]가 치환되서 발송됨
+						MMSCall(text);
 					} else if(msg.equals("<M2>")) {
 						// 문자 요청
+						String text = "[* 갱스터디 *] M2 !  즉시 지원 바랍니다. ";   // [필수] 문자내용 - 이름(names)값이 있다면 [*이름*]가 치환되서 발송됨
+						MMSCall(text);
 					} else if(msg.equals("<M3>")) {
 						// 문자 요청
+						String text = "[* 갱스터디 *] M3 !  즉시 지원 바랍니다. ";   // [필수] 문자내용 - 이름(names)값이 있다면 [*이름*]가 치환되서 발송됨
+						MMSCall(text);
 					} else {
 						System.out.println("["+LocalDateTime.now()+"]" + "From StudyRoom : " + msg);
 						RemoteLog log = new RemoteLog(msg, LocalDateTime.now(), RemoteLog.LogType.remote);
@@ -101,7 +108,32 @@ public class AdminWebSocketHandler extends TextWebSocketHandler implements Initi
 			}
 		}
 	}
+	
+	public void MMSCall(String msg) {
+		String userid = "neojts";           // [필수] 뿌리오 아이디             
+		String callback = "01021367733";    // [필수] 발신번호 - 숫자만
+		String phone = "01036372943";       // [필수] 수신번호 - 여러명일 경우 |로 구분 "010********|010********|010********"
+		String names = "";            // [선택] 이름 - 여러명일 경우 |로 구분 "홍길동|이순신|김철수"
+		String appdate = "";  // [선택] 예약발송 (현재시간 기준 10분이후 예약가능)
+		String subject = "뿌리오 문자 api 테스트";          // [선택] 제목 (30byte)
+		String file1Path = "";    // [선택]  포토발송 (jpg, jpeg만 지원  300 K  이하) 
+		try {
+			     
+		    SendMmsMessage sendMmsMessage = new SendMmsMessage();
+		    // filePath가 null 혹은 blank("")인 경우 일반 단/장문 발송. 
+			String response_str = sendMmsMessage.send( userid,  callback,  phone,  msg, names, appdate, subject, file1Path );
 
+			//response 
+			System.out.println("=============================");
+			System.out.println(response_str);
+			System.out.println("=============================");
+		} catch (IOException localIOException) {
+		    System.out.println(localIOException.toString());
+		} catch (Exception ex ){
+			ex.printStackTrace();
+		}
+	}
+	
 	// 웹소켓이 연결되면 호출되는 함수
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) 
