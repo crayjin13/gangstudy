2e2cddaba25c85c27ac0c0118a3442c6361d3876
apollyon4 <apollyon4@naver.com>
2021-02-19 18:22:14 +0900

    remove end date

--- src/main/webapp/resources/js/mainBook.js
+++ src/main/webapp/resources/js/mainBook.js
@@ -1,11 +1,14 @@
 
 const bookingForm = document.getElementById("bookingForm")
-const startDateInput = document.getElementById("start-date-input")
+const inputs = document.getElementsByClassName("form-control")
+
+const dateInput = document.getElementById("dateInput")
 const startTimeInput = document.getElementById("start-time-input")
-const endDateInput = document.getElementById("end-date-input")
 const endTimeInput = document.getElementById("end-time-input")
-const people = document.getElementById("people-input")
+const peopleInput = document.getElementById("people-input")
 const bookingButton = document.getElementById("bookingButton")
+
+const datePicker = getPikaday(dateInput)
 
 //
 //
@@ -16,28 +19,44 @@ var book_no = getParam("book_no")
 var startURL = "/booking/startTime"
 var endURL = "/booking/endTime"
 
+$(document).ready(function() {
+	// init calendar
+	setDateRange(datePicker, new Date(), 7)
+	
+	// error message
+	if(msg != "") { alert(msg); }
+})
+window.onpageshow = function() {
+	// info recovery
+	if(dateInput.value!="") {
+		requestTimes(startURL, getStartData(), startTimeInput, function() {
+			if(startTimeInput.value!="") {
+				requestTimes(endURL, getEndData(), endTimeInput)
+			}
+		})
+		
+		peopleInput.value = peopleInput.getAttribute("people")
+	} else {
+		dateInput.value = "이용날짜"
+	}
+}
+
 function getStartData() {
 	return {
-		"startDate" : startDateInput.value,
+		"date" : dateInput.value,
 		"book_no" : book_no
 	}
 }
 
 function getEndData() {
 	return {
-		"startDate" : startDateInput.value,
+		"date" : dateInput.value,
 		"startTime" : startTimeInput.value,
-		"endDate" : endDateInput.value,
 		"book_no" : book_no
 	}
 }
 
-var startPicker = getPikaday(startDateInput)
-var endPicker = getPikaday(endDateInput)
-
-
-
-
+// submit
 bookingButton.addEventListener("click", function() {
 	$.ajax({
 		url : "/booking/remove_uncharge",
@@ -49,67 +68,96 @@ bookingButton.addEventListener("click", function() {
 			console.log("ajax(remove_uncharge) error")
 		}
 	})
-	validationCheck(startDateInput.value, startTimeInput.value, endDateInput.value, endTimeInput.value, people.value, bookingForm)
-})
-
-$(document).ready(function() {
-	if(msg != "") { alert(msg); }
-	
-	setDateRange(startPicker, new Date(), 7)
-	setDateRange(endPicker, new Date(), 7)
-	
-	if(startDateInput.value!="") {
-		setDateRange(endPicker, new Date(startDateInput.value), 1)
-		
-		requestTimes(startURL, getStartData(), startTimeInput, function() {
-			if(startTimeInput.value!="") {
-				requestTimes(endURL, getEndData(), endTimeInput)
-			}
-		})
-		
-		people.value = people.getAttribute("people")
-	} else {
-		startDateInput.value = "이용날짜"
-		endDateInput.value = "시작일을 선택해주세요."
-	}
+	validationCheck(dateInput.value, startTimeInput.value, endTimeInput.value, peopleInput.value, bookingForm)
 })
 
 
 
-startDateInput.addEventListener("change", function() {
+
+// input change
+dateInput.addEventListener("change", function() {
 	removeOptions(startTimeInput)
 	removeOptions(endTimeInput)
-	
-	// end date select option
-	var date = new Date(startDateInput.value)
-	endDateInput.value = getFormatDate(date)
-	setDateRange(endPicker, date, 1)
 	
 	// start time select option
 	requestTimes(startURL, getStartData(), startTimeInput)
 })
-
-endDateInput.addEventListener("change", function() {
-	if(startDateInput.value=="") {
-		var date = new Date(endDateInput.value)
-		startDateInput.value = getFormatDate(date)
-		requestTimes(startURL, getStartData(), startTimeInput)
-	} else {
-		requestTimes(endURL, getEndData(), endTimeInput)
-	}
-});
 
 startTimeInput.addEventListener("change", function() {
 	if(startTimeInput.value == "") {
 		removeOptions(endTimeInput)
 		return
 	} else {
-		var time = getTimeObject(startTimeInput.value)
-		if(time.hour >= 22) {
-			var date = new Date(startDateInput.value)
-			date.setDate(date.getDate()+1)
-			endDateInput.value = getFormatDate(date)
-		}
 		requestTimes(endURL, getEndData(), endTimeInput)
 	}
 });
+
+
+
+// input focus
+var onFocued = function(i) {
+	var icon = document.getElementsByClassName("icon")
+	var o = icon[i].children[0].contentDocument
+	var s = o.getElementsByTagName("svg")[0]
+
+	var items = s.getElementsByClassName("paint")
+	for(var i = 0; i < items.length; i++) {
+		var item = items[i]
+		if(item.classList.contains("normal")) {
+			item.classList.remove("normal")
+		}
+		if(item.classList.contains("active")) {
+			item.classList.remove("active")
+		}
+		item.classList.add("clicked")
+	}
+	
+}
+dateInput.addEventListener("focus", function() {
+	onFocued(0)
+	dateInput.classList.add("active")
+})
+startTimeInput.addEventListener("focus",  function() {
+	onFocued(1)
+	startTimeInput.classList.add("active")
+})
+endTimeInput.addEventListener("focus",  function() {
+	onFocued(2)
+	endTimeInput.classList.add("active")
+})
+peopleInput.addEventListener("focus",  function() {
+	onFocued(3)
+	peopleInput.classList.add("active")
+})
+
+
+// input blur
+var onBlur = function(i) {
+	var icon = document.getElementsByClassName("icon")
+	var o = icon[i].children[0].contentDocument
+	var s = o.getElementsByTagName("svg")[0]
+	
+	var items = s.getElementsByClassName("paint")
+	for(var i = 0; i < items.length; i++) {
+		var item = items[i]
+		if(item.classList.contains("normal")) {
+			item.classList.remove("normal")
+		}
+		if(item.classList.contains("clicked")) {
+			item.classList.remove("clicked")
+		}
+		item.classList.add("active")
+	}
+}
+dateInput.addEventListener("blur", function() {
+	onBlur(0)
+})
+startTimeInput.addEventListener("blur",  function() {
+	onBlur(1)
+})
+endTimeInput.addEventListener("blur", function() {
+	onBlur(2)
+})
+peopleInput.addEventListener("blur", function() {
+	onBlur(3)
+})
