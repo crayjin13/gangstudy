c39208819f0e72b7ae2991bb2f123704870b3bde
apollyon4 <apollyon4@naver.com>
2021-03-16 14:55:32 +0900

    remotelog date change to datetime

--- src/main/java/com/jts/gangstudy/mapper/RemoteLogMapper.java
+++ src/main/java/com/jts/gangstudy/mapper/RemoteLogMapper.java
@@ -1,6 +1,6 @@
 package com.jts.gangstudy.mapper;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.jts.gangstudy.domain.RemoteLog;
@@ -8,5 +8,5 @@ import com.jts.gangstudy.domain.RemoteLog;
 public interface RemoteLogMapper {
 	public List<RemoteLog> selectAll();
 	public void insertLog(RemoteLog log);
-	public List<RemoteLog> selectByDate(LocalDate date);
+	public List<RemoteLog> selectByDateTime(LocalDateTime date);
 }
